17d9ae19bcbccf79d7824f9b76e8527863adacf4
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-11-04 20:18:21 -0600

    added 404

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req,res,next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="product"><button type="submit">Save</button>');
+    res.sendFile( path.join(rootDir, 'views', 'add-product.html') );
 });
 
 router.post('/add-product', (req,res,next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get( "/", (req, res, next) => {
-    // starting from the current folder, go up one level, then down into views
-    res.sendFile( path.join( __dirname, '../', 'views', 'shop.html' ));
+   res.sendFile( path.join( rootDir, 'views', 'shop.html' ));
 } );
 
 module.exports = router;
