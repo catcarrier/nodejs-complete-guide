2036b0d9a618facd0c924352d145ca15b4e68ddf
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-11-05 14:32:41 -0600

    started shared data

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,16 @@ const path = require('path');
 const rootDir = require('../util/path');
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req,res,next) => {
     res.sendFile( path.join(rootDir, 'views', 'add-product.html') );
 });
 
 router.post('/add-product', (req,res,next) => {
+    products.push({title:req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+module.exports.routes = router;
+module.exports.products = products;
